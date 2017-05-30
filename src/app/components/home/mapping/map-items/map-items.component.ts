e959b3a4c1b7fa1e7043d37e0164657a7613a0b5
kkayyoo <yokoyau159@gmail.com>
2017-05-30 12:25:17 -0400

    Outputting an array of map items w/ ngFor

--- src/app/components/home/mapping/map-items/map-items.component.ts
+++ src/app/components/home/mapping/map-items/map-items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Mapping } from '../mappings.model';
 
 @Component({
   selector: 'app-map-items',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map-items.component.scss']
 })
 export class MapItemsComponent implements OnInit {
+  mappingList: Mapping[] = [
+    new Mapping('http://maxpixel.freegreatpicture.com/static/photo/1x/Cocktail-Party-Recipe-Drink-Alcohol-Pisco-Sour-831768.jpg', 'ABC'),
+    new Mapping('http://maxpixel.freegreatpicture.com/static/photo/1x/Drink-Recipe-Pisco-Sour-Cocktail-Alcohol-Party-829477.jpg', 'XYZ'),
+    new Mapping('http://maxpixel.freegreatpicture.com/static/photo/1x/Cocktail-Party-Recipe-Drink-Alcohol-Pisco-Sour-831768.jpg', 'ABC'),
+    new Mapping('http://maxpixel.freegreatpicture.com/static/photo/1x/Drink-Recipe-Pisco-Sour-Cocktail-Alcohol-Party-829477.jpg', 'XYZ')
+  ];
 
   constructor() { }
 
---
+++ src/app/components/home/mapping/mappings.model.ts
@@ -0,0 +1,9 @@
+export class Mapping {
+    public imagePath: string;
+    public name: string;
+
+    constructor(imagePath: string, name: string) {
+        this.imagePath = imagePath;
+        this.name = name;
+    }
+}
