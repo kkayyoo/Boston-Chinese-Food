5c4f91a556392bc4688a2336ad139f4a3ace99e6
kkayyoo <yokoyau159@gmail.com>
2017-05-30 11:05:28 -0400

    Adding mapping component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeroComponent } from './components/home/hero/hero.component';
+import { MappingComponent } from './components/home/mapping/mapping.component';
+import { MapSearchComponent } from './components/home/mapping/map-search/map-search.component';
+import { MapItemsComponent } from './components/home/mapping/map-items/map-items.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HeroComponent
+    HeroComponent,
+    MappingComponent,
+    MapSearchComponent,
+    MapItemsComponent
   ],
   imports: [
     BrowserModule,
