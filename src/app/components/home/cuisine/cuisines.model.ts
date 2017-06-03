a2930e2b4e7d11884eac9347a2e10fac8015cac3
kkayyoo <yokoyau159@gmail.com>
2017-06-03 18:21:20 -0400

    Adding cuisine model

---
+++ src/app/components/home/cuisine/cuisines.model.ts
@@ -0,0 +1,11 @@
+export class FourCuisines {
+    public imagePath: string;
+    public name: string;
+    public info: string;
+
+    constructor(imagePath: string, name: string, info: string) {
+        this.imagePath = imagePath;
+        this.name = name;
+        this.info = info;
+    }
+}
