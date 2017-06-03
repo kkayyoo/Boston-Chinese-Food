8d8b0e6de1b7d6e699f4e8a4d4afd4d9576811a5
kkayyoo <yokoyau159@gmail.com>
2017-06-03 18:23:58 -0400

    Adding cuisine data model

--- src/app/components/home/cuisine/cuisine-items/cuisine-items.component.ts
+++ src/app/components/home/cuisine/cuisine-items/cuisine-items.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FourCuisines } from '../cuisines.model';
 
 @Component({
   selector: 'app-cuisine-items',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cuisine-items.component.scss']
 })
 export class CuisineItemsComponent implements OnInit {
+  cuisineList: FourCuisines[] = [
+    new FourCuisines('http://maxpixel.freegreatpicture.com/static/photo/1x/Cocktail-Party-Recipe-Drink-Alcohol-Pisco-Sour-831768.jpg', 'Shangdong Cuisine', 'info info info info info'),
+    new FourCuisines('http://maxpixel.freegreatpicture.com/static/photo/1x/Drink-Recipe-Pisco-Sour-Cocktail-Alcohol-Party-829477.jpg', 'Cantonese Cuisine', 'info info info info info'),
+    new FourCuisines('http://maxpixel.freegreatpicture.com/static/photo/1x/Cocktail-Party-Recipe-Drink-Alcohol-Pisco-Sour-831768.jpg', 'Sichuan Cuisine', 'info info info info info'),
+    new FourCuisines('http://maxpixel.freegreatpicture.com/static/photo/1x/Drink-Recipe-Pisco-Sour-Cocktail-Alcohol-Party-829477.jpg', 'Huiyang Cuisine', 'info info info info info')
+  ];
 
   constructor() { }
 
