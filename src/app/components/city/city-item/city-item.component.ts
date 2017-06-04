bf9075aa5fc68c857f9154e84888f716cf4b3bba
kkayyoo <yokoyau159@gmail.com>
2017-06-03 21:26:13 -0400

    Adding city page items

--- src/app/components/city/city-item/city-item.component.ts
+++ src/app/components/city/city-item/city-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cities } from '../cities.model';
 
 @Component({
   selector: 'app-city-item',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./city-item.component.scss']
 })
 export class CityItemComponent implements OnInit {
+  citiesList: Cities[] = [
+    new Cities('http://maxpixel.freegreatpicture.com/static/photo/1x/Cocktail-Party-Recipe-Drink-Alcohol-Pisco-Sour-831768.jpg', 'ABC'),
+    new Cities('http://maxpixel.freegreatpicture.com/static/photo/1x/Drink-Recipe-Pisco-Sour-Cocktail-Alcohol-Party-829477.jpg', 'XYZ'),
+    new Cities('http://maxpixel.freegreatpicture.com/static/photo/1x/Cocktail-Party-Recipe-Drink-Alcohol-Pisco-Sour-831768.jpg', 'ABC'),
+    new Cities('http://maxpixel.freegreatpicture.com/static/photo/1x/Drink-Recipe-Pisco-Sour-Cocktail-Alcohol-Party-829477.jpg', 'XYZ'),
+    new Cities('http://maxpixel.freegreatpicture.com/static/photo/1x/Cocktail-Party-Recipe-Drink-Alcohol-Pisco-Sour-831768.jpg', 'ABC'),
+    new Cities('http://maxpixel.freegreatpicture.com/static/photo/1x/Drink-Recipe-Pisco-Sour-Cocktail-Alcohol-Party-829477.jpg', 'XYZ')
+  ];
 
   constructor() { }
 
