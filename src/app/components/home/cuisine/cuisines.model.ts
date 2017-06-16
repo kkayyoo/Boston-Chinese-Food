3d9a82fb07bd9ff8af446861160b1c94182c4640
kkayyoo <yokoyau159@gmail.com>
2017-06-16 10:25:06 -0400

    Adding router elements to home cuisines

--- src/app/components/home/cuisine/cuisines.model.ts
+++ src/app/components/home/cuisine/cuisines.model.ts
@@ -2,10 +2,12 @@ export class FourCuisines {
     public imagePath: string;
     public name: string;
     public info: string;
+    public link: string;
 
-    constructor(imagePath: string, name: string, info: string) {
+    constructor(imagePath: string, name: string, info: string, link: string) {
         this.imagePath = imagePath;
         this.name = name;
         this.info = info;
+        this.link = link;
     }
 }
