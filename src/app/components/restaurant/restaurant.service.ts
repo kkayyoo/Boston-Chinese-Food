813ef3f02c13fa19e392d3c38754358fe36d4ff8
kkayyoo <yokoyau159@gmail.com>
2017-06-21 16:53:55 -0400

    Adding restaurant0 info

--- src/app/components/restaurant/restaurant.service.ts
+++ src/app/components/restaurant/restaurant.service.ts
@@ -8,12 +8,17 @@ export class RestaurantService {
     restaurantFood: FeaturedFood[] = [];
 
     private restaurantList: RestaurantDetail[] = [
-        new RestaurantDetail('Chilli Garden', 'Sichuan', '781-396-8488', '41 Riverside Ave, Medford, MA', '$$', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Cocktail-Party-Recipe-Drink-Alcohol-Pisco-Sour-831768.jpg',
+        new RestaurantDetail('Chilli Garden', 'Sichuan', '781-396-8488', '41 Riverside Ave, Medford, MA', '$$', '../../../../assets/images/restaurants/medford/ChilliGarden/chilli_garden.png',
+        '../../../../assets/images/restaurants/medford/ChilliGarden/map.png', '12:00 noon - 9:30 pm', '12:00 noon - 10:30 pm',
         [
-          new FeaturedFood('Double Cooked Pork Belly', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Cocktail-Party-Recipe-Drink-Alcohol-Pisco-Sour-831768.jpg', 'ABC'),
-          new FeaturedFood('234', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Drink-Recipe-Pisco-Sour-Cocktail-Alcohol-Party-829477.jpg', 'XYZ'),
+          new FeaturedFood('Double Cooked Pork Belly', '../../../../assets/images/restaurants/medford/ChilliGarden/pork.png', ''),
+          new FeaturedFood('Sliced Beef, Omasum & Tendon', '../../../../assets/images/restaurants/medford/ChilliGarden/feipian.png', ''),
+          new FeaturedFood('Sichuan Style Cold Noodle', '../../../../assets/images/restaurants/medford/ChilliGarden/liangmian.png', ''),
+          new FeaturedFood('Diced Chicken w/ House Made Pickles', '../../../../assets/images/restaurants/medford/ChilliGarden/chicken.png', ''),
+          
         ]),
         new RestaurantDetail('Brookline', 'Cantonese', '781-396-8488', '41 Riverside Ave, Medford, MA', '$$', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Cocktail-Party-Recipe-Drink-Alcohol-Pisco-Sour-831768.jpg',
+        '../../../../assets/images/restaurants/medford/ChilliGarden/map.png', '12:00 noon - 9:30 pm', '12:00 noon - 10:30 pm',
         [
           new FeaturedFood('123', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Cocktail-Party-Recipe-Drink-Alcohol-Pisco-Sour-831768.jpg', 'ABC'),
           new FeaturedFood('234', 'http://maxpixel.freegreatpicture.com/static/photo/1x/Drink-Recipe-Pisco-Sour-Cocktail-Alcohol-Party-829477.jpg', 'XYZ'),
--- src/app/components/restaurant/restaurants.model.ts
+++ src/app/components/restaurant/restaurants.model.ts
@@ -7,15 +7,21 @@ export class RestaurantDetail {
     public address: string;
     public price: string;
     public imagePath: string;
+    public map: string;
+    public hours1: string;
+    public hours2: string;
     public food: FeaturedFood[];
 
-    constructor(name: string, type: string, phone: string, address: string, price:string, imagePath: string, food: FeaturedFood[]) {
+    constructor(name: string, type: string, phone: string, address: string, price:string, imagePath: string, map: string, hours1: string, hours2: string, food: FeaturedFood[]) {
         this.name = name;
         this.type = type;
         this.phone = phone;
         this.address = address;
         this.price = price;
         this.imagePath = imagePath;
+        this.map = map;
+        this.hours1 = hours1;
+        this.hours2 = hours2;
         this.food = food;
     }
 }
